e1b71d52a06e103a886037943741fd0dadd7c98a
mrjahanbin <mrjahanbin90@gmail.com>
2022-07-13 08:03:52 +0430

    اضاف کردن متد MiladiYear

--- JanaPack/DateTimeConvertor.cs
+++ JanaPack/DateTimeConvertor.cs
@@ -6,6 +6,7 @@ namespace JanaPack
     public static class DateTimeConvertor
     {
 
+        #region Miladi
 
         public static DateTime ToMiladiDate(this string value)
         {
@@ -46,7 +47,49 @@ namespace JanaPack
             var Result = new DateTime(datePart[0], datePart[1], datePart[2], pc);
             return Result;
         }
+        public static string ToMiladiYear(this string value)
+        {
 
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return "";
+            }
+            if (value.Length > 10 || value.Length < 5)
+            {
+                return "";
+            }
+            var IsLetterOrDigit = value.All(Char.IsLetterOrDigit);
+            var IsSymbol = value.All(Char.IsSymbol);
+            //var IsHighSurrogate = value.All(Char.IsHighSurrogate);
+            var IsPunctuation = value.All(Char.IsPunctuation);
+            //var IsSeparator = value.All(Char.IsSeparator);
+            //var IsSurrogate = value.All(Char.IsSurrogate);
+            var IsWhiteSpace = value.Contains(" ");
+
+            if (IsLetterOrDigit || IsPunctuation || IsSymbol || IsWhiteSpace)
+            {
+                return "";
+            }
+
+            string ConvertPersianNumberToEng = Regex.Replace(value, "[۰-۹]", x => ((char)(x.Value[0] - '۰' + '0')).ToString());
+            int[]? datePart;
+            if (ConvertPersianNumberToEng.Contains(","))
+            {
+                datePart = ConvertPersianNumberToEng.Split(",").Select(d => int.Parse(d)).ToArray();
+            }
+            else
+            {
+                datePart = ConvertPersianNumberToEng.Split("/").Select(d => int.Parse(d)).ToArray();
+            }
+
+            PersianCalendar pc = new();
+            var Result = new DateTime(datePart[0], datePart[1], datePart[2], pc);
+            return Result.Year.ToString();
+        }
+        #endregion
+
+
+        #region Shamsi
         public static string ToShamsi(this DateTime value)
         {
 
@@ -67,7 +110,7 @@ namespace JanaPack
             var Result = pc.GetYear(value) + "/" + pc.GetMonth(value).ToString("00") + "/" + pc.GetDayOfMonth(value).ToString("00");
             return Result;
         }
-        
+
         public static string ToShamsiYear(this DateTime value)
         {
 
@@ -88,6 +131,6 @@ namespace JanaPack
             var Result = pc.GetYear(value).ToString();
             return Result;
         }
-
+        #endregion
     }
 }
--- JanaPackTest/ToMiladiTest.cs
+++ JanaPackTest/ToMiladiTest.cs
@@ -99,5 +99,100 @@ namespace JanaPackTest
             Assert.NotEqual(Expected, Act);
         }
         #endregion
+
+        #region ToMiladiYear
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("                        ")]
+        [InlineData(",,")]
+        [InlineData("^")]
+        [InlineData(",")]
+        [InlineData("~")]
+        [InlineData(".")]
+        [InlineData("//")]
+        [InlineData("099087987kjhghg")]
+        [InlineData("768768768")]
+        [InlineData("hgjhgjh")]
+        [InlineData("         kjhkj            ")]
+        [InlineData("         12213214            ")]
+        [InlineData("kjhkj         jhgjhgjhg")]
+        [InlineData("1765,76,57         2164352176")]
+        public void MiladiYear_Value_Not_Correct(string Input)
+        {
+            //arrange
+
+            //act
+            var Act = Fixer.Fix(Input).ToMiladiYear();
+
+            //assert
+            Assert.Equal("", Act);
+        }
+
+        [Theory]
+        [InlineData("1368,09,11")]
+        [InlineData("1368/09/11")]
+        [InlineData("1368,9,11")]
+        public void MiladiYear_Value_Correct(string Input)
+        {
+            //arrange
+            string Expected = "1989";
+
+            //act
+            var Act = Input.ToMiladiYear();
+
+            //assert
+            Assert.NotEqual("", Act);
+            Assert.Equal(Expected, Act);
+        }
+
+        [Theory]
+        [InlineData("168,9,11")]
+        public void MiladiYear_Value_Correct2(string Input)
+        {
+            //arrange0789-12-02
+            string Expected = "789";
+
+            //act
+            var Act = Input.ToMiladiYear();
+
+            //assert
+            Assert.NotEqual("", Act);
+            Assert.Equal(Expected, Act);
+        }
+
+        [Theory]
+        [InlineData("1,1,1")]
+        public void MiladiYear_Value_Correct3(string Input)
+        {
+            //arrange0622-03-22
+            string Expected = "622";
+
+            //act
+            var Act = Input.ToMiladiYear();
+
+            //assert
+            Assert.NotEqual("", Act);
+            Assert.Equal(Expected, Act);
+        }
+
+        [Theory]
+        [InlineData("68,9,11")]
+        [InlineData("1399,12,30")]
+        [InlineData("68,9,1")]
+        public void MiladiYear_Value_Correct4(string Input)
+        {
+            //arrange0622-03-22
+            string Expected = "1";
+
+            //act
+            var Act = Input.ToMiladiYear();
+
+            //assert
+            Assert.NotEqual("", Act);
+            Assert.NotEqual(Expected, Act);
+        }
+        #endregion
     }
 }
