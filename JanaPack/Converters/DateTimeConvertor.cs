3e6abfcd2bd193db173b36612271df3437cc60f5
mrjahanbin <mrjahanbin90@gmail.com>
2022-07-13 16:25:26 +0430

    متد ToMiladiDatetime اضاف شد

--- JanaPack/Converters/DateTimeConvertor.cs
+++ JanaPack/Converters/DateTimeConvertor.cs
@@ -35,39 +35,100 @@ namespace JanaPack.Converters
             {
                 datePart = ConvertPersianNumberToEng.Split(",").Select(d => int.Parse(d)).ToArray();
             }
+            else if (ConvertPersianNumberToEng.Contains('-'))
+            {
+                datePart = ConvertPersianNumberToEng.Split("-").Select(d => int.Parse(d)).ToArray();
+            }
             else
             {
                 datePart = ConvertPersianNumberToEng.Split("/").Select(d => int.Parse(d)).ToArray();
             }
 
+
             PersianCalendar pc = new();
+
+            if (datePart[0] > 9999 || datePart[1] > 12 || datePart[1] > 31)
+            {
+                return new();
+            }
+
+
             var Result = new DateTime(datePart[0], datePart[1], datePart[2], pc);
             return Result;
         }
         public static DateTime ToMiladiDatetime(this string value)
         {
-            if (string.IsNullOrWhiteSpace(value))
-            {
-                return new();
-            }
-            if (value.Length > 10 || value.Length < 5)
-            {
-                return new();
-            }
-            var IsLetterOrDigit = value.All(char.IsLetterOrDigit);
-            var IsSymbol = value.All(char.IsSymbol);
-            var IsPunctuation = value.All(char.IsPunctuation);
-            var IsWhiteSpace = value.Contains(' ');
+            DateTime DateOnly = new();
+            List<int>? TimeOnly = null;
 
-            if (IsLetterOrDigit || IsPunctuation || IsSymbol || IsWhiteSpace)
+            value = Fixer.Fix(value);
+            var Value = value.Split(" ");
+            var Date = Value[0];
+            if (!Date.Contains(':'))
             {
-                return new();
+                DateOnly = Date.ToMiladiDate();
             }
-            string ConvertPersianNumberToEng = Regex.Replace(value, "[۰-۹]", x => ((char)(x.Value[0] - '۰' + '0')).ToString());
-            var datePart = ConvertPersianNumberToEng.Split(new[] { '-', ':', ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(d => int.Parse(d)).ToArray();
+
+            if (Value.Length > 1 || Value[0].Contains(':'))
+            {
+                var Time = Value[0].Contains(':') == true ? Value[0] : Value[1];
+                if (Time.Contains('.'))
+                {
+                    var SplitTime = Time.Split('.');
+                    Time = SplitTime[0];
+                }
+
+                if (string.IsNullOrWhiteSpace(Time) || Time.Length > 22 || Time.Length < 5)
+                {
+                    return new();
+                }
+
+                var IsLetterOrDigit = Time.All(char.IsLetterOrDigit);
+                var IsNumber = !Time.Contains(':');
+                var IsLetter = Time.Replace(" ", "").All(char.IsLetter);
+                var IsSymbol = Time.All(char.IsSymbol);
+                var IsPunctuation = Time.All(char.IsPunctuation);
+                var IsWhiteSpace = Time.Count(Char.IsWhiteSpace) > 2;
+
+                if (IsLetterOrDigit || IsPunctuation || IsSymbol || IsWhiteSpace || IsLetter || IsNumber)
+                {
+                    return new();
+                }
+                string ConvertPersianNumberToEng = Regex.Replace(Time, "[۰-۹]", x => ((char)(x.Value[0] - '۰' + '0')).ToString());
+                ConvertPersianNumberToEng = ConvertPersianNumberToEng.Replace("AM", "").Replace("PM", "");
+
+                TimeOnly = ConvertPersianNumberToEng.Split(new[] { ':', ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(d => int.Parse(d)).ToList();
+                if (TimeOnly.Count == 2)
+                {
+                    TimeOnly.Add(0);
+                }
+
+                if (TimeOnly[0] > 24 || TimeOnly[1] > 60 || TimeOnly[2] > 60)
+                {
+                    if (DateOnly.Year != 1)
+                    {
+                        return DateOnly;
+                    }
+                    else
+                    {
+                        return new();
+                    }
+                }
+
+            }
 
             PersianCalendar pc = new();
-            var Result = new DateTime(datePart[0], datePart[1], datePart[2], datePart[3], datePart[4], datePart[5], pc);
+            DateTime Result = new();
+
+            if (TimeOnly != null)
+            {
+                Result = new(DateOnly.Year, DateOnly.Month, DateOnly.Day, TimeOnly[0], TimeOnly[1], TimeOnly[2]);
+            }
+            else
+            {
+                Result = new(DateOnly.Year, DateOnly.Month, DateOnly.Day);
+            }
+
             return Result;
 
         }
@@ -76,14 +137,11 @@ namespace JanaPack.Converters
         public static string ToMiladiYear(this string value)
         {
 
-            if (string.IsNullOrWhiteSpace(value))
+            if (string.IsNullOrWhiteSpace(value) || value.Length > 10 || value.Length < 5)
             {
                 return "";
             }
-            if (value.Length > 10 || value.Length < 5)
-            {
-                return "";
-            }
+
             var IsLetterOrDigit = value.All(char.IsLetterOrDigit);
             var IsSymbol = value.All(char.IsSymbol);
             var IsPunctuation = value.All(char.IsPunctuation);
--- JanaPackTest/Converters/DateTimes/ToMiladiTest.cs
+++ JanaPackTest/Converters/DateTimes/ToMiladiTest.cs
@@ -100,6 +100,275 @@ namespace JanaPackTest.Converters.DateTimes
         }
         #endregion
 
+        #region ToMiladiDateTime
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("                        ")]
+        [InlineData(",,")]
+        [InlineData("^")]
+        [InlineData(",")]
+        [InlineData("~")]
+        [InlineData(".")]
+        [InlineData("//")]
+        [InlineData("099087987kjhghg")]
+        [InlineData("768768768")]
+        [InlineData("hgjhgjh")]
+        [InlineData("         kjhkj            ")]
+        [InlineData("         12213214            ")]
+        [InlineData("kjhkj         jhgjhgjhg")]
+        [InlineData("1765,76,57")]
+        [InlineData("1765,76,57         2164352176")]
+        public void DateTimeValue_Not_Correct(string Input)
+        {
+            //arrange
+            DateTime Expected = new(1, 1, 1, 0, 0, 0);
+
+            //act
+            var Act = Input.ToMiladiDatetime();
+
+            //assert
+            Assert.Equal(Expected.Date, Act.Date);
+            Assert.Equal(Expected.Hour, Act.Hour);
+            Assert.Equal(Expected.Minute, Act.Minute);
+            Assert.Equal(Expected.Second, Act.Second);
+        }
+
+        [Theory]
+        [InlineData("1368,09,11")]
+        [InlineData("1368,9,11")]
+        [InlineData("1368/09/11")]
+        [InlineData("1368/9/11")]
+        [InlineData("1368-09-11")]
+        [InlineData("1368-9-11")]
+        public void DateTimeValue_Correct(string Input)
+        {
+            //arrange
+            DateTime Expected = new(1989, 12, 02);
+
+            //act
+            var Act = Input.ToMiladiDatetime();
+
+            //assert
+            Assert.Equal(Expected, Act);
+        }
+
+        [Theory]
+        [InlineData("1368,09,11 08:37:15 AM")]
+        [InlineData("1368,9,11 08:37:15 AM")]
+        [InlineData("1368/09/11 08:37:15 AM")]
+        [InlineData("1368/9/11 08:37:15 AM")]
+        [InlineData("1368-09-11 08:37:15 AM")]
+        [InlineData("1368-9-11 08:37:15 AM")]
+        public void DateTimeValue_Correct8(string Input)
+        {
+            //arrange
+            DateTime Expected = new(1989, 12, 02, 08, 37, 15);
+
+            //act
+            var Act = Input.ToMiladiDatetime();
+
+            //assert
+            Assert.Equal(Expected.Date, Act.Date);
+            Assert.Equal(Expected.Hour, Act.Hour);
+            Assert.Equal(Expected.Minute, Act.Minute);
+            Assert.Equal(Expected.Second, Act.Second);
+        }
+
+        [Theory]
+        [InlineData("9368,09,11 98:97:95 AM")]
+        [InlineData("9368,9,11 98:97:95 AM")]
+        [InlineData("9368/09/11 98:97:95 AM")]
+        [InlineData("9368/9/11 98:97:95 AM")]
+        [InlineData("9368-09-11 98:97:95 AM")]
+        [InlineData("9368-9-11 98:97:95 AM")]
+        public void DateTimeValue_Correct9(string Input)
+        {
+            //arrange
+            DateTime Expected = new(9989, 11, 28, 0, 0, 0);
+
+            //act
+            var Act = Input.ToMiladiDatetime();
+
+            //assert
+            Assert.Equal(Expected.Date, Act.Date);
+            Assert.Equal(Expected.Hour, Act.Hour);
+            Assert.Equal(Expected.Minute, Act.Minute);
+            Assert.Equal(Expected.Second, Act.Second);
+        }
+
+
+
+
+        [Theory]
+        [InlineData("11:31:16 AM")]
+        [InlineData("11:31:16")]
+        public void DateTimeValue_Correct_Time(string Input)
+        {
+            //arrange
+            DateTime Expected = new(1, 1, 1, 11, 31, 16);
+
+            //act
+            var Act = Input.ToMiladiDatetime();
+
+            //assert
+            Assert.Equal(Expected.Date, Act.Date);
+            Assert.Equal(Expected.Hour, Act.Hour);
+            Assert.Equal(Expected.Minute, Act.Minute);
+            Assert.Equal(Expected.Second, Act.Second);
+        }
+
+
+        [Theory]
+        [InlineData("91:91:96 AM")]
+        [InlineData("91:91:96")]
+        public void DateTimeValue_Correct_Time3(string Input)
+        {
+            //arrange
+            DateTime Expected = new(1, 1, 1, 0, 0, 0);
+
+            //act
+            var Act = Input.ToMiladiDatetime();
+
+            //assert
+            Assert.Equal(Expected.Date, Act.Date);
+            Assert.Equal(Expected.Hour, Act.Hour);
+            Assert.Equal(Expected.Minute, Act.Minute);
+            Assert.Equal(Expected.Second, Act.Second);
+        }
+
+        [Theory]
+        [InlineData("11:31 AM")]
+        [InlineData("11:31")]
+        public void DateTimeValue_Correct_Time2(string Input)
+        {
+            //arrange
+            DateTime Expected = new(1, 1, 1, 11, 31, 0);
+
+            //act
+            var Act = Input.ToMiladiDatetime();
+
+            //assert
+            Assert.Equal(Expected.Date, Act.Date);
+            Assert.Equal(Expected.Hour, Act.Hour);
+            Assert.Equal(Expected.Minute, Act.Minute);
+            Assert.Equal(Expected.Second, Act.Second);
+        }
+
+        [Fact]
+        public void DateTimeValue_Correct5()
+        {
+            //arrange
+            string Input = "1401/12/20 11:31:16 AM";
+            DateTime Expected = new(2023, 03, 11, 11, 31, 16);
+
+            //act
+            var Act = Input.ToMiladiDatetime();
+
+            //assert
+            Assert.Equal(Expected.Date, Act.Date);
+            Assert.Equal(Expected.Hour, Act.Hour);
+            Assert.Equal(Expected.Minute, Act.Minute);
+            Assert.Equal(Expected.Second, Act.Second);
+        }
+
+        [Fact]
+        public void DateTimeValue_Correct10()
+        {
+            //arrange
+            string Input = "1/1/1 12:00:00 AM";
+            DateTime Expected = new(622, 03, 22, 12, 0, 0);
+
+            //act
+            var Act = Input.ToMiladiDatetime();
+
+            //assert
+            Assert.Equal(Expected.Date, Act.Date);
+            Assert.Equal(Expected.Hour, Act.Hour);
+            Assert.Equal(Expected.Minute, Act.Minute);
+            Assert.Equal(Expected.Second, Act.Second);
+        }
+
+        [Fact]
+        public void DateTimeValue_Correct11()
+        {
+            //arrange
+            string Input = "01-01-01 12:00:00.9545454";
+            DateTime Expected = new(622, 03, 22, 12, 0, 0);
+
+            //act
+            var Act = Input.ToMiladiDatetime();
+
+            //assert
+            Assert.Equal(Expected.Date, Act.Date);
+            Assert.Equal(Expected.Hour, Act.Hour);
+            Assert.Equal(Expected.Minute, Act.Minute);
+            Assert.Equal(Expected.Second, Act.Second);
+        }
+        
+        [Fact]
+        public void DateTimeValue_Correct12()
+        {
+            //arrange
+            string Input = "01-01-01 22:00:00.9545454";
+            DateTime Expected = new(622, 03, 22, 22, 0, 0);
+
+            //act
+            var Act = Input.ToMiladiDatetime();
+
+            //assert
+            Assert.Equal(Expected.Date, Act.Date);
+            Assert.Equal(Expected.Hour, Act.Hour);
+            Assert.Equal(Expected.Minute, Act.Minute);
+            Assert.Equal(Expected.Second, Act.Second);
+        }
+
+        [Theory]
+        [InlineData("168,9,11")]
+        public void DateTimeValue_Correct2(string Input)
+        {
+            //arrange0789-12-02
+            DateTime Expected = new(789, 12, 02);
+
+            //act
+            var Act = Input.ToMiladiDatetime();
+
+            //assert
+            Assert.Equal(Expected, Act);
+        }
+
+        [Theory]
+        [InlineData("1,1,1")]
+        public void DateTimeValue_Correct3(string Input)
+        {
+            //arrange0622-03-22
+            DateTime Expected = new(622, 3, 22);
+
+            //act
+            var Act = Input.ToMiladiDatetime();
+
+            //assert
+            Assert.Equal(Expected, Act);
+        }
+
+        [Theory]
+        [InlineData("68,9,11")]
+        [InlineData("1399,12,30")]
+        [InlineData("68,9,1")]
+        public void DateTimeValue_Correct4(string Input)
+        {
+            //arrange0622-03-22
+            DateTime Expected = new(1, 1, 1);
+
+            //act
+            var Act = Input.ToMiladiDatetime();
+
+            //assert
+            Assert.NotEqual(Expected, Act);
+        }
+        #endregion
+
         #region ToMiladiYear
         [Theory]
         [InlineData(null)]
