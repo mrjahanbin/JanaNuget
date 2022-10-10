6f62c772629813d847d12df52a281b0a36b80ace
mrjahanbin90 <mrjahanbin90@gmail.com>
2022-10-10 14:08:28 +0330

    Add Math

---
+++ JanaPack/Calculates/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JanaPack.Calculates
+{
+    public static class Math
+    {
+        public static decimal Percentage(this decimal value, decimal Percentage)
+        {
+            var decimalResult = (value * Percentage)/100;
+            return decimalResult;
+        }
+    }
+}
---
+++ JanaPackTest/Calculates/MathTest.cs
@@ -0,0 +1,34 @@
+﻿using JanaPack.Calculates;
+using JanaPack.Converters;
+
+namespace JanaPackTest.Converters.Numbers
+{
+    /*
+     * میخوایم تست کنیم ببینیم تاریخ فارسی رو چطوری به میلادی تبدیل کنیم   
+     */
+    public class MathTest
+    {
+        #region MathTest
+
+        [Theory]
+        [InlineData(2000, 10)]
+        [InlineData(2000, 0)]
+        [InlineData(2000, 1000)]
+        [InlineData(20, 1000)]
+        public void Value_Correct(decimal Input, decimal Percentage)
+        {
+            //arrange
+            var Expected = (Input*Percentage)/100;
+
+            //act
+            var Act = Input.Percentage(Percentage);
+
+            //assert
+            Assert.Equal(Expected, Act);
+
+        }
+        #endregion
+
+
+    }
+}
